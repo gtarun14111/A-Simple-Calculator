2006e03934a8e6f356203643ea6c27356333185a
Tarun <gtarun14111@gmail.com>
2020-05-23 21:25:52 +0530

    Updates

---
+++ precache-manifest.604631cd1f71c518eebddfc81a072781.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a720346d963c9ca3ca440e7edd6ac3fe",
+    "url": "/gtarun14111/A-Simple-Calculator/index.html"
+  },
+  {
+    "revision": "524cb2a3bcba841a89ac",
+    "url": "/gtarun14111/A-Simple-Calculator/static/css/main.8fb1257d.chunk.css"
+  },
+  {
+    "revision": "ed7dff189b2b335c7b80",
+    "url": "/gtarun14111/A-Simple-Calculator/static/js/2.8e48544d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/gtarun14111/A-Simple-Calculator/static/js/2.8e48544d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "524cb2a3bcba841a89ac",
+    "url": "/gtarun14111/A-Simple-Calculator/static/js/main.28231a29.chunk.js"
+  },
+  {
+    "revision": "e1120aaef83f1f9248cc",
+    "url": "/gtarun14111/A-Simple-Calculator/static/js/runtime-main.0f4267b8.js"
+  }
+]);
